a8048c1dfa32a69330bd9a2670ac36abb31d5532
daniel rammer <dnl.rammer@gmail.com>
2025-12-29 20:21:10 +0100

    CHANGED: pi wifi IP

--- copySoundsToPi.sh
+++ copySoundsToPi.sh
@@ -1,2 +1,2 @@
  #!/bin/bash
- rsync -av --exclude=SystemSoundsProject Sounds dnl@192.168.0.251:/home/dnl/RasPlayer
+ rsync -av --exclude=SystemSoundsProject Sounds dnl@192.168.0.70:/home/dnl/RasPlayer
