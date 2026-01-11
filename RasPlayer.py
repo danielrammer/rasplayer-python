3ee0339c8d16c24ae71a450d4df974e61971c1c8
danielra <dnl.rammer@gmail.com>
2026-01-11 14:18:12 +0100

    CHANGED: auto play next song

--- RasPlayer.py
+++ RasPlayer.py
@@ -207,6 +207,7 @@ GPIO.add_event_detect(Input.INPUT_INSTRUMENT_MODE, GPIO.RISING, callback=lambda 
 
 # loop until termination
 while True:
-    if soundPlayer is SamplePlayer:
-        SamplePlayer(soundPlayer).update()
-    sleep(100)
+    # if soundPlayer is SamplePlayer:
+    #     SamplePlayer(soundPlayer).update()
+    soundPlayer.update()
+    sleep(0.1)
